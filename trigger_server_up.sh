95764bccf731a496ebc59bf134862c04b533ba18
shahzaib-123 <shahasghar054@gmail.com>
2023-12-23 20:30:19 +0500

    updated the trigger up bash stcript to map port 5000 to port 80

--- trigger_server_up.sh
+++ trigger_server_up.sh
@@ -3,13 +3,9 @@
 IMAGE_NAME="flaskapp"
 
 sudo docker run -p 3306:3306 --name db -v /home/ubuntu/db_volume:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=test -d mariadb
-if docker image inspect "$IMAGE_NAME" &> /dev/null; then
+if sudo docker image inspect "$IMAGE_NAME" &> /dev/null; then
     echo "Removing existing Docker image: $IMAGE_NAME"
-    docker image rm "$IMAGE_NAME"
+    sudo docker image rmi "$IMAGE_NAME"
 fi
 sudo docker build -t flaskapp .
-sudo docker run -p 5000:5000 --name flaskapp flaskapp
-
-
-
-
+sudo docker run -d -p 80:5000 --name flaskapp flaskapp
